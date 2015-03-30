0dd7a618294d2df4d519bd4bd35d0c0befcf7728
Bruce Whealton <futurewavewebdevelopment@gmail.com>
2015-03-30 10:41:08 -0400

    Initial Commit

---
+++ another.js
@@ -0,0 +1,22 @@
+(function() {
+  "use strict";
+
+}());
+
+
+
+ (function() {
+   "use strict";
+
+   var MyModule = (function() {
+         function MyModule() {
+             test = 0;
+         }
+         MyModule.prototype = {
+
+         };
+
+         return MyModule;
+     }());
+
+ }());
